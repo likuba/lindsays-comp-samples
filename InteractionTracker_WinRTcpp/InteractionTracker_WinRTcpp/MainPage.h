bce00ea9732031d250dea04b54f7a333bcabc06c
Lindsay Kubasik <likuba@microsoft.com>
2018-12-06 17:02:53 -0800

    Initial Commit
    pushing some initial projects

---
+++ InteractionTracker_WinRTcpp/InteractionTracker_WinRTcpp/MainPage.h
@@ -0,0 +1,29 @@
+﻿//
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::InteractionTracker_WinRTcpp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+
+		void InitializeComposition();
+    };
+}
+
+namespace winrt::InteractionTracker_WinRTcpp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
